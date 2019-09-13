9a96acf80293a444b47ec30e5848936281830ed0
chrisli8 <8chrisgli@gmail.com>
2019-09-13 11:35:37 -0700

    pre add enemy penguin

---
+++ Whack-a-Penguin/WhackSlot.swift
@@ -0,0 +1,54 @@
+//
+//  WhackSlot.swift
+//  Whack-a-Penguin
+//
+//  Created by Chris Li on 9/12/19.
+//  Copyright © 2019 Chris Li. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class WhackSlot: SKNode {
+    
+    var charNode: SKSpriteNode!
+    var isVisible = false
+    var isHit = false
+    var popupTime = 0.85
+    
+    
+    func configure(at position: CGPoint) {
+        self.position = position
+        
+        let sprite = SKSpriteNode(imageNamed: "whackHole")
+        addChild(sprite)
+        
+        let cropNode = SKCropNode()
+        cropNode.position = CGPoint(x: 0, y: 15)
+        cropNode.zPosition = 1
+        cropNode.maskNode = SKSpriteNode(imageNamed: "whackMask")
+        
+        charNode = SKSpriteNode(imageNamed: "penguinGood")
+        charNode.position = CGPoint(x: 0, y: -90)
+        charNode.name = "character"
+        cropNode.addChild(charNode)
+        
+        addChild(cropNode)
+    }
+    
+    func show(hideTime: Double) {
+        if isVisible { return }
+        charNode.run(SKAction.moveBy(x: 0, y: 80, duration: 0.05))
+        isVisible = true
+        isHit = false
+        
+        if Int.random(in: 0...2) == 0 {
+            charNode.texture = SKTexture(imageNamed: "penguinGood")
+            charNode.name = "charFriend"
+        } else {
+            charNode.texture = SKTexture(imageNamed: "penguinEvil")
+            charNode.name = "charEnemy"
+        }
+    }
+
+}
